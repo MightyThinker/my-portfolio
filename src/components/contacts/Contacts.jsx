89024e0d44b2f9688619edd1361e8e56b1d9f3c9
PRATYUSH KUMAR <pratyush1002@gmail.com>
2025-08-15 00:07:30 +0530

    feat: dialog added for confirmation

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react';
 import './Contacts.css';
 import emailjs from '@emailjs/browser';
-import { motion } from 'motion/react';
+import { AnimatePresence, motion } from 'motion/react';
 import ContactSvg from './ContactSvg';
 import { useSectionInView } from '@reusable/useSectionInView';
 
@@ -10,7 +10,22 @@ export default function Contacts() {
   const { ref, controls } = useSectionInView({ threshold: 0.3 });
   const myForm = useRef();
 
-  const [message, setMessage] = useState('');
+  const [dialogMessage, setDialogMessage] = useState('');
+  const [dialogType, setDialogType] = useState('success'); // success | error
+  const dialogRef = useRef(null);
+
+  const handleDialogClose = () => {
+    dialogRef.current?.close();
+    myForm.current?.reset(); // clear form inputs
+    setDialogMessage('');
+  };
+
+  // Modal animation
+  const dialogVariants = {
+    hidden: { scale: 0.8, opacity: 0 },
+    visible: { scale: 1, opacity: 1, transition: { duration: 0.3, ease: "easeOut" } },
+    exit: { scale: 0.8, opacity: 0, transition: { duration: 0.2, ease: "easeIn" } }
+  };
 
   // Zoom-in from background
   const cSectionLeft = {
@@ -55,71 +70,100 @@ export default function Contacts() {
     visible: { scale: 1, opacity: 1, transition: { duration: 0.5 } }
   };
 
+  // Email Sending for message from viewer
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs
       .sendForm(
-        import.meta.env.VITE_SERVICE_ID, 
-        import.meta.env.VITE_TEMPLATE_ID, 
-        myForm.current, 
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        myForm.current,
         {
           publicKey: import.meta.env.VITE_PUBLIC_KEY,
         }
       )
       .then(
         () => {
-          setMessage("Your message has been sent.");
+          setDialogType('success');
+          setDialogMessage("Your message has been sent.");
+          dialogRef.current?.showModal();
           console.log('SUCCESS!');
         },
         (error) => {
-          setMessage("You message can not be sent!");
-          console.log('FAILED...', error.text, error);
+          setDialogType('error');
+          setDialogMessage("Your message could not be sent!");
+          dialogRef.current?.showModal();
+          console.log('FAILED...', error?.text);
         },
       );
   }
 
   return (
-    <div ref={ref} className="contacts">
+    <React.Fragment>
+      <div ref={ref} className="contacts">
 
-      {/* Left Section */}
-      <motion.div variants={cSectionLeft} animate={controls} className="cSection">
+        {/* Left Section */}
+        <motion.div variants={cSectionLeft} animate={controls} className="cSection">
 
-        {/* Form */}
-        <motion.form variants={formSection} animate={controls} ref={myForm} onSubmit={sendEmail}>
-          <motion.h1 variants={formItem} className='cTitle'>Let's keep in touch.</motion.h1>
-          <motion.div variants={formItem} className="form-item">
-            <label htmlFor="name">Name</label>
-            <input name='user_name' required type="text" id="name" placeholder='John Doe' />
-          </motion.div>
-          <motion.div variants={formItem} className="form-item">
-            <label htmlFor="email">Email</label>
-            <input name='user_email' required type="email" id="email" placeholder='john.doe@example.com' />
-          </motion.div>
-          <motion.div variants={formItem} className="form-item">
-            <label htmlFor="message">Message</label>
-            <textarea name='user_message' required rows={5} id="message" placeholder='Write your message...' />
-          </motion.div>
-          <motion.button variants={formItem} className="form-button">Send</motion.button>
-        </motion.form>
-        <span>{message}</span>
+          {/* Form */}
+          <motion.form variants={formSection} animate={controls} ref={myForm} onSubmit={sendEmail}>
+            <motion.h1 variants={formItem} className='cTitle'>Let's keep in touch.</motion.h1>
+            <motion.div variants={formItem} className="form-item">
+              <label htmlFor="name">Name</label>
+              <input name='user_name' required type="text" id="name" placeholder='John Doe' />
+            </motion.div>
+            <motion.div variants={formItem} className="form-item">
+              <label htmlFor="email">Email</label>
+              <input name='user_email' required type="email" id="email" placeholder='john.doe@example.com' />
+            </motion.div>
+            <motion.div variants={formItem} className="form-item">
+              <label htmlFor="message">Message</label>
+              <textarea name='user_message' required rows={5} id="message" placeholder='Write your message...' />
+            </motion.div>
+            <motion.button variants={formItem} className="form-button">Send</motion.button>
+          </motion.form>
 
-        {/* Other Social Connection Option */}
-        <motion.h4 className='cTitle' variants={formItem}>or, you can connect with me on</motion.h4>
-        <motion.div variants={formItem} className="social">
-          <a href="https://www.linkedin.com/in/mighty-thinker/" target="_blank" rel="noopener noreferrer">
-            <img src='https://img.icons8.com/fluency/40/linkedin.png' alt='LinkedIn' />
-          </a>
-          <a href="https://www.instagram.com/_pratyush_kumar_/" target="_blank" rel="noopener noreferrer">
-            <img src='https://img.icons8.com/fluency/40/instagram-new.png' alt='Instagram' />
-          </a>
+          {/* Other Social Connection Option */}
+          <motion.h4 className='cTitle' variants={formItem}>or, you can connect with me on</motion.h4>
+          <motion.div variants={formItem} className="social">
+            <a href="https://www.linkedin.com/in/mighty-thinker/" target="_blank" rel="noopener noreferrer">
+              <img src='https://img.icons8.com/fluency/40/linkedin.png' alt='LinkedIn' />
+            </a>
+            <a href="https://www.instagram.com/_pratyush_kumar_/" target="_blank" rel="noopener noreferrer">
+              <img src='https://img.icons8.com/fluency/40/instagram-new.png' alt='Instagram' />
+            </a>
+          </motion.div>
         </motion.div>
-      </motion.div>
 
-      {/* Animated SVG */}
-      <motion.div variants={cSectionRight} animate={controls} className="cSection">
-        <ContactSvg />
-      </motion.div>
-    </div>
+        {/* Animated SVG */}
+        <motion.div variants={cSectionRight} animate={controls} className="cSection">
+          <ContactSvg />
+        </motion.div>
+      </div>
+      {/* Native Dialog */}
+      <AnimatePresence>
+        <dialog
+          ref={dialogRef}
+          className={`dialog-box ${dialogType}`}
+          onClick={(e) => {
+            if (e.target === dialogRef.current) {
+              handleDialogClose();
+            }
+          }}
+        >
+          <motion.div
+            className='dialog-content'
+            variants={dialogVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+          >
+            <p>{dialogMessage}</p>
+            <button className={`dialog-btn ${dialogType}`} onClick={handleDialogClose}>OK</button>
+          </motion.div>
+        </dialog>
+      </AnimatePresence>
+    </React.Fragment>
   )
 }
